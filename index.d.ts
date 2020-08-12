81b69d176eee6a347bca741bd3054d05aeac0db0
Robert Pisani <robert.pisani@compassdigital.io>
2020-08-11 21:46:44 -0400

    BO-2382 [Add] Module declaration and overloaded default export
    BO-2382 [Fix] Union undefined with ID response types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,21 @@
-export default function CompassDigitalId(
-  data: string,
-  provider: string,
-  type: string,
-  id: string
-): string;
+declare module '@compassdigital/id' {
+	export type CdlEncodedID = string;
+
+	export interface CdlDecodedID {
+		service: string;
+		provider: string;
+		type: string;
+		id: string;
+	}
+
+	function ID(
+		service: string,
+		provider: string,
+		type: string,
+		id: string
+	): CdlEncodedID | undefined;
+	function ID(decoded: CdlDecodedID): CdlEncodedID | undefined;
+	function ID(encoded: CdlEncodedID): CdlDecodedID | undefined;;
+
+	export default ID;
+}
